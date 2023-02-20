b5736311e94297480bc0bc583149e2c5c5fbc82b
ra312 <akylzhanov.r@gmail.com>
2023-02-20 09:55:07 +0100

    test: added tests for predict dummy predict endpoint

---
+++ tests/test_server.py
@@ -0,0 +1,20 @@
+# import json
+from fastapi.testclient import TestClient
+from model_server import InferenceServer
+
+
+def test_predict_endpoint():
+    server = InferenceServer(name="test")
+    client = TestClient(server.app)
+    response = client.get("/predict")
+    assert response.status_code == 200
+    data = response.json()
+    assert "relevance_scores" in data
+    assert data["relevance_scores"] == "test"
+
+
+def test_missing_endpoint():
+    server = InferenceServer(name="test")
+    client = TestClient(server.app)
+    response = client.get("/nonexistent")
+    assert response.status_code == 404
