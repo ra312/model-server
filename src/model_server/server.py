7c93bbadcfcf0d44ce7e604c48dbb852cfd0bcba
rauan_akylzhanov <akylzhanov.r@gmail.com>
2023-02-20 10:58:48 +0100

    Added dummpy predict endpoint with tests (#4)
    * ci: added github ci workflow
    
    * poetry: added initial files
    
    * WIP: do not merge, going to container due to lightgbm macos m1 issues
    
    * test: added tests for predict dummy predict endpoint
    
    * Added dummpy predict endpoint with tests
    
    	* ci: added github ci workflow
    	* poetry: added initial files
    	* test: added tests for predict dummy predict endpoint, coverage 89
    	* prehooks: code is linted
    
    * ci: allow coverage > 70

---
+++ src/model_server/server.py
@@ -0,0 +1,22 @@
+# It's a class that creates a Flask app and allows you to add endpoints to it
+from typing import Dict
+
+import uvicorn
+from fastapi import APIRouter, FastAPI
+
+
+class InferenceServer:
+    def __init__(self, name: str):
+        self.name = name
+        self.app = FastAPI()
+        self.router = APIRouter()
+        self.router.add_api_route("/predict", self.predict, methods=["GET"])
+        self.app.include_router(self.router)
+
+    def predict(self) -> Dict[str, str]:
+        return {"relevance_scores": self.name}
+
+
+if __name__ == "__main__":
+    server = InferenceServer(name="recommend-venues")
+    uvicorn.run(server.app, host="localhost", port=8000)
