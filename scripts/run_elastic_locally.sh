a5d6bef6b30925d26c4d2fdf355fc42c8770c9b6
rauan_akylzhanov <akylzhanov.r@gmail.com>
2023-03-18 22:40:10 +0000

    added elasticsearch and kibana

--- scripts/run_elastic_locally.sh
+++ scripts/run_elastic_locally.sh
@@ -1,19 +1,27 @@
 #!/bin/bash -e
 
 function cleanup {
-    echo "Removin elastic search container"
-    docker rm /elasticsearch
+    echo "Removing Elasticsearch and Kibana containers"
+    docker stop elasticsearch kibana
+    docker rm elasticsearch kibana
 }
+
+echo "Stopping any running Elasticsearch and Kibana containers"
+docker stop elasticsearch kibana || true
+docker rm elasticsearch kibana || true
 
 echo "pulling elasticsearch:7.13.3 image";
 docker pull docker.elastic.co/elasticsearch/elasticsearch:7.13.3;
+
+echo "pulling kibana:7.13.3 image";
+docker pull docker.elastic.co/kibana/kibana:7.13.3;
 
 echo "creating docker network for elastic and kibana"
 # create if not exists
 # https://stackoverflow.com/a/48643576/5444759
 docker network create --driver bridge elastic || true;
 
-echo "starting elastic"
+echo "starting Elasticsearch"
 docker run \
     --name elasticsearch \
     --net elastic \
@@ -21,6 +29,35 @@ docker run \
     -e discovery.type=single-node \
     -e ES_JAVA_OPTS="-Xms1g -Xmx1g" \
     -e xpack.security.enabled=false \
-    -it docker.elastic.co/elasticsearch/elasticsearch:7.13.3;
+    -d docker.elastic.co/elasticsearch/elasticsearch:7.13.3;
+
+echo "starting Kibana"
+docker run \
+    --name kibana \
+    --net elastic \
+    -p 5601:5601 \
+    -e ELASTICSEARCH_HOSTS=http://elasticsearch:9200 \
+    -d docker.elastic.co/kibana/kibana:7.13.3;
 
 trap cleanup EXIT;
+
+echo "Waiting for Elasticsearch and Kibana to start up..."
+sleep 30
+
+echo "Creating index in Elasticsearch"
+curl -X PUT "localhost:9200/my_index" -H 'Content-Type: application/json' -d '
+{
+    "settings": {
+        "number_of_shards": 1,
+        "number_of_replicas": 0
+    }
+}'
+
+echo "Indexing sample data"
+curl -X POST "localhost:9200/my_index/_doc" -H 'Content-Type: application/json' -d '
+{
+  "message": "Hello World"
+}'
+
+echo "Accessing Kibana UI"
+echo "Please navigate to http://localhost:5601 in your web browser"
