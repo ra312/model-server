f5f8c2bba9eb9a1813a9fff6b3bf43f4711bfee2
rauan_akylzhanov <akylzhanov.r@gmail.com>
2023-03-20 11:39:37 +0100

    Dev/add locust (#25)
    * remove unused dep
    
        pyproject.toml: remove jupyter dependencies
        tests: remove warnings by
            using latest version sort signature for polars dataframe
    
    * WIP: adding load tests
    
    * addest load tests
    
     * dev: added locust load tests for 1M users
     * docs: updated readme (how to run tests)
     * cicd: temp switched off load tests from cicd,
        reason: need to start server in the background mode
    
    * pre-commit: linted testt_inference_request_time.py
    
    * pyproject: added invokust>=0.77
    
    * ignore inference service tests
    
        * ci: inference service tests are ignored (need to run server as a
        fixture)
        * ci: always fail Github Action if at least one pre-commit hook fails
    
    * pr: sync poetry.lock with main version

---
+++ tests/test_inference_request_time.py
@@ -0,0 +1,41 @@
+import json
+import logging
+import time
+
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+
+
+def test_send_inference_request(app: FastAPI, request_body: dict) -> None:
+    """
+    Test the speed and request size of the model's inference.
+
+    This test sends a POST request to the model's endpoint using a test client, with a request
+    body containing a single JSON object. The elapsed time and request size in kilobytes are
+    logged using the Python logging module. The test passes if the response status code is 200.
+
+    To run this test, you need to have a running instance of the model endpoint.
+
+    Example usage: `pytest -s test_inference_speed_and_request_size.py`
+
+    Args:
+        app (FastAPI): A FastAPI instance with a single endpoint for generating predicted venue ratings.
+    """
+
+    number_of_inference_requests = 100
+    request_body_json = json.dumps([request_body] * number_of_inference_requests)
+    request_body_bytes = request_body_json.encode()
+
+    headers = {"accept": "application/json", "Content-Type": "application/json"}
+
+    with TestClient(app) as client:
+        start_time = time.monotonic()
+        response = client.post("/predict", headers=headers, content=request_body_bytes)
+        elapsed_time = time.monotonic() - start_time
+        request_size_kb = len(request_body_bytes) / 1024
+        logging.info("Number of inference requests: %s", number_of_inference_requests)
+        logging.info(
+            f"Request size: {request_size_kb:.2f} KB, Elapsed time: {elapsed_time:.4f} seconds"
+        )
+        assert 0.01 <= elapsed_time <= 0.04
+        assert response.status_code == 200
